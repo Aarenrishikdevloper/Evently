92206d75adea7d2d0630e4182dd800f00ec0bf44
Aarenrishikdevloper <you@example.com>
2023-12-19 10:15:21 +0530

    first commit

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,13 +1,20 @@
+import mongoose from 'mongoose';
 
-import mongoose from "mongoose";
-let cached =(global as any). momgoose || {conn:null, promise:null};
-export const connectTodatabase = async()=>{
-    const MONGO_URI = process.env.MONGO_URI;
-    if(cached.conn) return cached.conn;
-    if(!MONGO_URI) throw new Error("NONGODB_URI is missing");
-    cached.promise = cached.promise || mongoose.connect(MONGO_URI, {dbName:"Eventapp", bufferCommands:false});
+const MONGODB_URI = process.env.MONGODB_URI;
 
-    cached.conn =await cached.promise;
+let cached = (global as any).mongoose || { conn: null, promise: null };
+
+export const connectToDatabase = async () => {
+    if (cached.conn) return cached.conn;
+
+    if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
+
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+        dbName: 'evently',
+        bufferCommands: false,
+    })
+
+    cached.conn = await cached.promise;
+
     return cached.conn;
-
 }
