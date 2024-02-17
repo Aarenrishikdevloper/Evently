303cce19187254954725e8f14e74b29c7ac2a4c9
Rishik Kashyap <115730685+Aarenrishikdevloper@users.noreply.github.com>
2024-02-17 09:54:44 +0530

    Update page.tsx

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -24,7 +24,7 @@ const DetailPage = async({params:{id}, searchParams}:SearchParamProps) => {
         <section className='flex justify-center bg-primary-50 bg-dotted-pattern bg-contain'>
            <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl"  >
               
-               <Image src={event.imageUrl} alt="HeroImage" width={1000} height={1000}  className={"h-full min-w-[300px]  object-cover object-center" }/>
+               <Image src={event.imageUrl} alt="HeroImage" width={1000} height={1000}  className={"h-full min-h-[300px]  object-cover object-center" }/>
                
                <div className="flex w-full flex-col gap-8 p-5 md:p-10">
                    <div className="flex flex-col gap-6">
